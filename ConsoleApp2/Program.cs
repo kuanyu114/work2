7ec36152c8838f6304c27e19dcc306f59d63ab7d
Kuan Tsai <ktsai11486@gmail.com>
2022-11-24 21:04:55 +0800

    修改命名

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -7,23 +7,24 @@ int[] list1 = new int[] { 1, 0, 2, 4, 0, 5, 7 };
 int[] list2 = new int[] { 1, 7, 2, 0, 4, 5, 0 };
 int[] list3 = new int[] { 1, 7, 2, 7, 4, 5, 0 };
 
-bool result = new newFunction().checkArry(list);
+bool result = new NewFunction().CheckArry(list);
 Console.WriteLine("result1" + result);
-result = new newFunction().checkArry(list1);
+result = new NewFunction().CheckArry(list1);
 Console.WriteLine("result2" + result);
-result = new newFunction().checkArry(list2);
+result = new NewFunction().CheckArry(list2);
 Console.WriteLine("result3" + result);
-result = new newFunction().checkArry(list3);
+result = new NewFunction().CheckArry(list3);
 Console.WriteLine("result4" + result);
 
-public class newFunction
+public class NewFunction
 {
-    public bool checkArry(int[] ints)
+    public bool CheckArry(int[] ints)
     {
         int[] checklist = new int[] { 0, 0, 7 };
         bool result = false;
         int count = 0;
         int index = 0;
+                
         foreach (var item in checklist)
         {
             for (int i = index; i < ints.Length; i++)
